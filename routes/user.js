c069820e6fb750bce15109d4962305f6ec858e4c
Mohamed Zayan <m7mdzayan@gmail.com>
2021-05-27 16:41:39 +0200

    add password encryption

--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { User, validateUser } = require("../models/User");
 const userRouter = express.Router();
+const bcrypt = require("bcrypt");
 
 userRouter.post("/register", async (req, res) => {
   const { error } = validateUser(req.body);
   if (error) return res.status(400).send({ message: error.details[0].message });
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user)
+    return res.status(400).send({ message: "user already registered." });
 
   user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
   user = await user.save();
 
   res.send({
